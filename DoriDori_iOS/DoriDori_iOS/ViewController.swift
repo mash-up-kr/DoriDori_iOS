1f0b5d1d79a09f4e671d549945a7dafc8d687503
JeongMinho <wjdalsgh95@nate.com>
2022-06-15 23:56:49 +0900

    ✨  : init project

---
+++ DoriDori_iOS/DoriDori_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoriDori_iOS
+//
+//  Created by JeongMinho on 2022/06/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
