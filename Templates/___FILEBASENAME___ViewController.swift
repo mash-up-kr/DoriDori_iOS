124c2aed8d0cc8fb2be9bef2e2e3ba33197ea597
JeongMinho <wjdalsgh95@nate.com>
2022-06-19 00:27:15 +0900

    🎨  : reactor template 추가

---
+++ Templates/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,31 @@
+//___FILEHEADER___
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+final class ___VARIABLE_productName___ViewController: UIViewController {
+
+    var disposeBag = DisposeBag()
+
+    // MARK: - Life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Bind ViewModel
+
+    func bind(viewModel: ___VARIABLE_productName___ViewModel) {
+        bindAction(viewModel: viewModel)
+        bindState(viewModel: viewModel)
+    }
+
+    func bindAction(viewModel: ___VARIABLE_productName___ViewModel) {
+        
+    }
+
+    func bindState(viewModel: ___VARIABLE_productName___ViewModel) {
+        
+    }
+}
