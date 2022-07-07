397db5cd57309019342dfc3d7ddc473a3403241c
JeongMinho <wjdalsgh95@nate.com>
2022-07-07 23:08:20 +0900

    :art: [#13] color extension 추가

---
+++ DoriDori_iOS/DoriDori_iOS/Common/Extensions/UIColor+Extension.swift
@@ -0,0 +1,38 @@
+//
+//  UIColor+Extension.swift
+//  DoriDori_iOS
+//
+//  Created by JeongMinho on 2022/07/07.
+//
+
+import UIKit
+
+extension UIColor {
+    static let lime900: UIColor = UIColor(named: "lime900")!
+    static let lime800: UIColor = UIColor(named: "lime800")!
+    static let lime700: UIColor = UIColor(named: "lime700")!
+    static let lime600: UIColor = UIColor(named: "lime600")!
+    static let lime500: UIColor = UIColor(named: "lime500")!
+    static let lime400: UIColor = UIColor(named: "lime400")!
+    static let lime300: UIColor = UIColor(named: "lime300")!
+    static let lime200: UIColor = UIColor(named: "lime200")!
+    static let lime100: UIColor = UIColor(named: "lime100")!
+    
+    static let gray900: UIColor = UIColor(named: "gray900")!
+    static let gray800: UIColor = UIColor(named: "gray800")!
+    static let gray700: UIColor = UIColor(named: "gray700")!
+    static let gray600: UIColor = UIColor(named: "gray600")!
+    static let gray500: UIColor = UIColor(named: "gray500")!
+    static let gray400: UIColor = UIColor(named: "gray400")!
+    static let gray300: UIColor = UIColor(named: "gray300")!
+    static let gray200: UIColor = UIColor(named: "gray200")!
+    static let gray100: UIColor = UIColor(named: "gray100")!
+    
+    static let red900: UIColor = UIColor(named: "red900")!
+    static let red700: UIColor = UIColor(named: "red700")!
+    static let red500: UIColor = UIColor(named: "red500")!
+    static let red300: UIColor = UIColor(named: "red300")!
+    static let red100: UIColor = UIColor(named: "red100")!
+    
+    static let darkGray: UIColor = UIColor(named: "darkGray")!
+}
