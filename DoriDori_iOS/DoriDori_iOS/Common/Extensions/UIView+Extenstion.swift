6db0a25cf55ddd2b2952c64de15dd7833ae5bc52
kimjiin <kj980926@naver.com>
2022-07-09 21:48:23 +0900

    :art: [#8] EmailLogin 화면 구성

---
+++ DoriDori_iOS/DoriDori_iOS/Common/Extensions/UIView+Extenstion.swift
@@ -0,0 +1,40 @@
+//
+//  UIView+Extenstion.swift
+//  DoriDori_iOS
+//
+//  Created by 김지인 on 2022/07/09.
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = true
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
---
+++ DoriDori_iOS/DoriDori_iOS/Scene/SignIn/EmailLoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  EmailLoginViewController.swift
+//  DoriDori_iOS
+//
+//  Created by 김지인 on 2022/07/09.
+//
+
+import UIKit
+
+class EmailLoginViewController: UIViewController {
+
+    @IBOutlet weak var emailPwFindStackView: UIStackView!
+    @IBOutlet weak var emailSignUpButton: UIButton!
+    @IBOutlet weak var emailFindButton: UIButton!
+    
+    @IBOutlet weak var passwordFindButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+
+
+}
