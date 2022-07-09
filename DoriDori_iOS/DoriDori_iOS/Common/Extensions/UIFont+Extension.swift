b6d863e7789d0d7d5f1dbdb1ab40ecaf07ec35bf
seoyoung <bgd216@naver.com>
2022-07-09 23:00:15 +0900

    ✨ [#29] 폰트 추가

---
+++ DoriDori_iOS/DoriDori_iOS/Common/Extensions/UIFont+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  UIFont+Extension.swift
+//  DoriDori_iOS
+//
+//  Created by Seori on 2022/07/09.
+//
+
+import UIKit
+
+extension UIFont {
+    static let SFPro: String = "SFPro"
+    static let SpoqaHanSansNeo: String = "SpoqaHanSansNeo"
+}
