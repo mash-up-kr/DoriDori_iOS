9c8e0c80f52d939b819c01d7b5c7164c49441e59
JeongMinho <wjdalsgh95@nate.com>
2022-07-09 17:35:37 +0900

    🧱 [#12] appCoordinator 추가

---
+++ DoriDori_iOS/DoriDori_iOS/AppStart/AppCoordinator.swift
@@ -0,0 +1,53 @@
+//
+//  AppCoordinator.swift
+//  DoriDori_iOS
+//
+//  Created by JeongMinho on 2022/07/09.
+//
+
+import UIKit
+
+final class AppCoordinator: Coordinator {
+
+    private let appStart: AppStart
+    private let window: UIWindow?
+    private let tabbarItems: [HomeTabType] = HomeTabType.allCases
+
+    init(appStart: AppStart, window: UIWindow) {
+        self.appStart = appStart
+        self.window = window
+    }
+
+    private func createTabBarViewControllers(tab: HomeTabType) -> UIViewController {
+        let viewController: UIViewController
+
+        switch tab {
+        // TODO: 각 VC에서 자신의 Coordinator 생성해서 주입해줘야 됨.
+        case .first:
+            let firstViewController = ViewController()
+            viewController = firstViewController
+        case .second:
+            let secondViewController = ViewController()
+            viewController = secondViewController
+        case .third:
+            let thirdViewController = ViewController()
+            viewController = thirdViewController
+        }
+
+        viewController.tabBarItem.title = tab.tabTitle
+        viewController.tabBarItem.image = tab.tabImage
+        viewController.tabBarItem.selectedImage = tab.selectedImage
+        return viewController
+    }
+
+    func start() {
+        switch appStart {
+        case .home:
+            let homeTabbarController = HomeTabBarController()
+            let viewControllers = tabbarItems.map(createTabBarViewControllers(tab:))
+            homeTabbarController.setViewControllers(viewControllers, animated: false)
+            window?.rootViewController = homeTabbarController
+        }
+    }
+}
+
