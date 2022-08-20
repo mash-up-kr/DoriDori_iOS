6a3646018f9af030b5f51cf80139adcb87b15bd3
seoyoung <bgd216@naver.com>
2022-08-20 20:42:09 +0900

    빌드 오류 수정

--- DoriDori_iOS/DoriDori_iOS/Common/APIs/AnswerCompleteRequest.swift
+++ DoriDori_iOS/DoriDori_iOS/Common/APIs/AnswerCompleteRequest.swift
@@ -9,8 +9,21 @@ import Foundation
 
 // 본인
 struct MyAnswerCompleteRequest: Requestable {
-    var path: String { "/api/v1/user/answered-questions?lastId=\(self.lastQuestionID)&size=\(self.size)" }
-    var parameters: Parameter? { nil }
+    private let size: Int
+    private let lastQuestionID: QuestionID?
+    var path: String { "/api/v1/user/answered-questions" }
+    var parameters: Parameter? {
+        if let lastQuestionID = self.lastQuestionID {
+            return [
+                "lastId": lastQuestionID,
+                "size": self.size
+            ]
+        } else {
+            return [
+                "size": self.size
+            ]
+        }
+    }
     init(lastQuestionID: QuestionID?, size: Int) {
         self.lastQuestionID = lastQuestionID
         self.size = size
@@ -19,8 +32,25 @@ struct MyAnswerCompleteRequest: Requestable {
 
 // 다른사람
 struct AnswerCompleteRequest: Requestable {
-    var path: String { "/api/v1/questions/answered?userId=\(self.userID)&lastId=\(self.lastQuestionID)&size=\(self.size)" }
-    var parameters: Parameter? { nil }
+    private let userID: UserID
+    private let lastQuestionID: QuestionID?
+    private let size: Int
+    
+    var path: String { "/api/v1/questions/answered" }
+    var parameters: Parameter? {
+        if let lastQuestionID = lastQuestionID {
+            return [
+                "userId": self.userID,
+                "lastId": lastQuestionID,
+                "size": self.size
+            ]
+        } else {
+            return [
+                "userId": self.userID,
+                "size": self.size
+            ]
+        }
+    }
     init(userID: UserID, lastQuestionID: QuestionID?, size: Int) {
         self.userID = userID
         self.lastQuestionID = lastQuestionID
