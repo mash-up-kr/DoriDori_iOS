f61a99d6f592e95cfce4f2aa261d9b743aa5e0f3
seoyoung <bgd216@naver.com>
2022-08-20 20:26:11 +0900

    ✨ toast view UI

---
+++ DoriDori_iOS/DoriDori_iOS/Scene/Common/DoriDoriToastView.swift
@@ -0,0 +1,68 @@
+//
+//  DoriDoriToastView.swift
+//  DoriDori_iOS
+//
+//  Created by Seori on 2022/08/20.
+//
+
+import Toast
+import UIKit
+import SnapKit
+
+final class DoriDoriToastView: UIView {
+    
+    private let textLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = UIFont.setKRFont(weight: .regular, size: 13)
+        return label
+    }()
+    
+    private let duration: TimeInterval
+    
+    init(
+        text: String,
+        duration: TimeInterval = 4.0
+    ) {
+        self.duration = duration
+        super.init(frame: CGRect(x: 0, y: 0, width: 256, height: 38))
+        self.textLabel.text = text
+        self.setupLayouts()
+        self.backgroundColor = UIColor(red: 37/255, green: 37/255, blue: 37/255, alpha: 0.8)
+        self.layer.cornerRadius = 4
+        self.clipsToBounds = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func show() {
+        guard let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate
+                as? SceneDelegate,
+              let tabBar = sceneDelegate.window?.rootViewController as? UITabBarController else {
+            return
+        }
+        let y = (UIScreen.main.bounds.height - (tabBar.tabBar.bounds.height + 20))
+        let x = UIScreen.main.bounds.width / 2
+        sceneDelegate.window?.showToast(
+            self,
+            duration: self.duration,
+            point: CGPoint(x: x, y: y),
+            completion: nil
+        )
+    }
+}
+
+extension DoriDoriToastView {
+    private func setupLayouts() {
+        self.addSubview(self.textLabel)
+        self.textLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(10)
+            $0.bottom.equalToSuperview().inset(10)
+            $0.leading.equalToSuperview().offset(32)
+            $0.trailing.equalToSuperview().inset(32)
+        }
+    }
+}
