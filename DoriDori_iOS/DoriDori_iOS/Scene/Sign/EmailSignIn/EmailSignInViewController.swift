8a30f25bbf68ea7f934493c53aabd9c921fd9740
julia0926 <kj980926@naver.com>
2022-08-22 20:39:40 +0900

    ✨ [#67] 로그인 API 연결

--- DoriDori_iOS/DoriDori_iOS/Scene/Sign/EmailSignIn/EmailSignInViewController.swift
+++ DoriDori_iOS/DoriDori_iOS/Scene/Sign/EmailSignIn/EmailSignInViewController.swift
@@ -18,18 +18,21 @@ final class EmailSignInViewController: UIViewController {
     @IBOutlet private weak var emailFindButton: UIButton!
     @IBOutlet private weak var passwordFindButton: UIButton!
     @IBOutlet private weak var loginButtomConstraint: NSLayoutConstraint!
-    @IBOutlet private weak var loginBUtton: UIButton!
+    @IBOutlet private weak var loginButton: UIButton!
     
     private let keyboardUpButtomConstraint: CGFloat = 20
     private let keyboardDownButtomConstraint: CGFloat = 54
 
-    var disposeBag = DisposeBag()
+    private let viewModel: EmailSignInViewModel = .init()
+    private var disposeBag = DisposeBag()
 
     // MARK: - Life cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         keyboardSetting()
+        settingViewModel()
+        bind(viewModel)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -38,19 +41,42 @@ final class EmailSignInViewController: UIViewController {
     }
 
     // MARK: - Bind ViewModel
-
-    func bind(viewModel: EmailSignInViewModel) {
-
+    private func settingViewModel() {
+        emailTextField.viewModel = UnderLineTextFieldViewModel(titleLabelType: .email,
+                                                               inputContentType: .emailAddress,
+                                                               returnKeyType: .default,
+                                                               keyboardType: .emailAddress)
+        passwordTextField.viewModel = UnderLineTextFieldViewModel(titleLabelType: .password,
+                                                                    inputContentType: .password,
+                                                                    returnKeyType: .default,
+                                                                    keyboardType: .default)
     }
     
-    @IBAction func tapLoginButton(_ sender: UIButton) {
-        guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "TermsOfServiceViewContoller") as? TermsOfServiceViewContoller
-        else { return }
-        navigationController?.pushViewController(vc, animated: true)
-    }
-    
+    private func bind(_ viewModel: EmailSignInViewModel) {
+        let input = EmailSignInViewModel.Input(email: emailTextField.textField.rx.text.orEmpty.asObservable(),
+                                               password: passwordTextField.textField.rx.text.orEmpty.asObservable(),
+                                               loginButtonTap: loginButton.rx.tap.asObservable())
+        
+        let output = viewModel.transform(input: input)
+        output.buttonIsValid.bind { [weak self] isValid in
+            self?.loginButton.isEnabled = isValid
+            self?.loginButton.backgroundColor = isValid ? UIColor(named: "lime300") : UIColor(named: "gray700")
+            let buttonTitleColor = isValid ? UIColor(named: "darkGray") : UIColor(named: "gray300")
+            self?.loginButton.setTitleColor(buttonTitleColor, for: .normal)
+        }.disposed(by: disposeBag)
+        
+        output.signIn.bind { [weak self]_ in
+            self?.navigationController?.pushViewController(HomeViewController(), animated: true)
+        }.disposed(by: disposeBag)
+        
+        output.errorMessage.emit(onNext: { [weak self] str in
+            let alert = UIAlertController(title: "로그인", message: str, preferredStyle: .alert)
+            let confirmAction = UIAlertAction(title: "확인", style: .default)
+            alert.addAction(confirmAction)
+            self?.present(alert, animated: true)
+        }).disposed(by: disposeBag)
 
-    
+    }
 }
 
 //MARK: - textField 편집시 Keyboard 설정
--- DoriDori_iOS/DoriDori_iOS/Scene/Sign/EmailSignIn/EmailSignInViewModel.swift
+++ DoriDori_iOS/DoriDori_iOS/Scene/Sign/EmailSignIn/EmailSignInViewModel.swift
@@ -6,19 +6,50 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class EmailSignInViewModel: ViewModelProtocol {
+final class EmailSignInViewModel: ViewModelProtocol {
+    
+    private let repository: EmailSignInRepository = .init()
+    
     struct Input {
-        
+        let email: Observable<String>
+        let password: Observable<String>
+        let loginButtonTap: Observable<Void>
     }
     
     struct Output {
-        
-        
+        let buttonIsValid: Observable<Bool>
+        let signIn: Observable<Void>
+        let errorMessage: Signal<String>
     }
     
     func transform(input: Input) -> Output {
-        return Output()
+        
+        let buttonIsValid = Observable.combineLatest(input.email, input.password) { email, pw in
+            return email.emailValidCheck && pw.passwordValidCheck
+        }
+        
+        let errorRelay = PublishRelay<String>()
+        let signIn = input.loginButtonTap.withLatestFrom(Observable.combineLatest(input.email, input.password) { ($0, $1) })
+            .flatMapLatest { [weak self] email, password -> Observable<Void> in
+                guard let self = self else { return .empty() }
+                return self.repository.requestLogin(email: email, loginType: "BASIC", password: password)
+                    .catch { error in
+                        guard let errorModel = error.toErrorModel else { return .empty() }
+                        if errorModel.code == "LOGIN_FAILED" {
+                            guard let errorMsg = errorModel.message else {
+                                return .empty()
+                            }
+                            errorRelay.accept(errorMsg)
+                        }
+                        return .empty()
+                    }.map({ _ in
+                        return
+                    })}.observe(on: MainScheduler.instance)
+                
+        return Output(buttonIsValid: buttonIsValid, signIn: signIn, errorMessage: errorRelay.asSignal())
     }
     
 }
