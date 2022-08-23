a3e5d25bd61ad32f21c669b92cff23aa2f519c03
seoyoung <bgd216@naver.com>
2022-08-24 00:50:18 +0900

    ✨ [#75] 리프레시하는 로직 추가

--- DoriDori_iOS/DoriDori_iOS/Common/UserDefault/UserDefault+Value.swift
+++ DoriDori_iOS/DoriDori_iOS/Common/UserDefault/UserDefault+Value.swift
@@ -7,6 +7,17 @@
 
 import Foundation
 
+typealias AccessToken = String
+typealias RefreshToken = String
+
 extension UserDefaults {
     // 앞으로 여기에 key 값으로 추가 부탁드릴게요!
+    @UserDefault(key: "accessToken", defaultValue: "")
+    static var accessToken: AccessToken
+    
+    @UserDefault(key: "refreshToken", defaultValue: "")
+    static var refreshToken: RefreshToken
+    
+    @UserDefault(key: "userID", defaultValue: "")
+    static var userID: UserID
 }
