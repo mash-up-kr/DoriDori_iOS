a11d1ffcf0303bbed06601151c96efb55ca452d1
seoyoung <bgd216@naver.com>
2022-08-25 00:59:28 +0900

    Merge branch 'develop' into feature/question

--- DoriDori_iOS/DoriDori_iOS/Common/UserDefault/UserDefault+Value.swift
+++ DoriDori_iOS/DoriDori_iOS/Common/UserDefault/UserDefault+Value.swift
@@ -7,6 +7,17 @@
 
 import Foundation
 
+typealias AccessToken = String
+typealias RefreshToken = String
+
 extension UserDefaults {
     // 앞으로 여기에 key 값으로 추가 부탁드릴게요!
+    @UserDefault(key: "accessToken", defaultValue: nil)
+    static var accessToken: AccessToken?
+    
+    @UserDefault(key: "refreshToken", defaultValue: nil)
+    static var refreshToken: RefreshToken?
+    
+    @UserDefault(key: "userID", defaultValue: nil)
+    static var userID: UserID?
 }
