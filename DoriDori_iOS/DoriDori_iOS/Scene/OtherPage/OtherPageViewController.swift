6656aab37bdeb923d9a4dcf4e6237caf8489fa95
seoyoung <bgd216@naver.com>
2022-08-24 22:32:56 +0900

    ✨ [#71] 상대방 프로필, 구분선, contentViewController 생성 및 레이아웃 구성

--- DoriDori_iOS/DoriDori_iOS/Scene/OtherPage/OtherPageViewController.swift
+++ DoriDori_iOS/DoriDori_iOS/Scene/OtherPage/OtherPageViewController.swift
@@ -9,22 +9,53 @@ import Foundation
 import UIKit
 
 final class OtherPageViewController: UIViewController {
-    let profileView = OtherProfileView(frame: .zero)
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.addSubViews(profileView)
-        self.view.backgroundColor = .gray900
-        self.navigationController?.navigationBar.isHidden = true
-        profileView.snp.makeConstraints {
-            $0.top.equalTo(self.view.safeAreaLayoutGuide)
-            $0.leading.trailing.equalToSuperview()
-        }
-        self.profileView.configure(OtherProfileItem(nickname: "도리를찾아서", level: 1, profileImageURL: nil, description: "ㅇㄴ녕하세요 디즈니 영화 다 추천해드려요", tags: ["디즈니", "영화", "애니메이션"], representativeWard: "강남구"))
-    }
+    // MARK: - UIComponents
     
+    private let profileView = OtherProfileView()
+    private let profileDividerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray800
+        return view
+    }()
+    
+    // MARK: - Properties
+
     override var hidesBottomBarWhenPushed: Bool {
         get { self.navigationController?.topViewController == self }
         set { super.hidesBottomBarWhenPushed = newValue }
+    }
+
+    // MARK: - LifeCycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupLayouts()
+        self.setupUI()
+        self.profileView.configure(OtherProfileItem(nickname: "도리를찾아서", level: 1, profileImageURL: nil, description: "ㅇㄴ녕하세요 디즈니 영화 다 추천해드려요", tags: ["디즈니", "영화", "애니메이션"], representativeWard: "강남구"))
+    }
+
+    private func setupLayouts() {
+        let contentViewController = OtherProfileContentViewController()
+        
+        self.view.addSubViews(self.profileView, self.profileDividerView, contentViewController.view)
+        self.profileView.snp.makeConstraints {
+            $0.top.equalTo(self.view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+        }
+        self.profileDividerView.snp.makeConstraints {
+            $0.top.equalTo(self.profileView.snp.bottom)
+            $0.height.equalTo(1)
+            $0.leading.trailing.equalToSuperview()
+        }
+        contentViewController.view.snp.makeConstraints {
+            $0.top.equalTo(self.profileDividerView.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    private func setupUI() {
+        self.view.backgroundColor = .gray900
+        self.navigationController?.navigationBar.isHidden = true
     }
 }
---
+++ DoriDori_iOS/DoriDori_iOS/Scene/OtherPage/OtherProfileContentViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OtherProfileContentViewController.swift
+//  DoriDori_iOS
+//
+//  Created by Seori on 2022/08/24.
+//
+
+import UIKit
+
+
+final class OtherProfileContentViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .darkGray
+    }
+}
