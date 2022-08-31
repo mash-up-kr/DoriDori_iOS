c0eca865fd4883a210ad698775b0f63271495e27
julia0926 <kj980926@naver.com>
2022-08-31 14:36:14 +0900

    :bug: accessToken 고정키 값 제거

--- DoriDori_iOS/DoriDori_iOS/Common/UserDefault/UserDefault+Value.swift
+++ DoriDori_iOS/DoriDori_iOS/Common/UserDefault/UserDefault+Value.swift
@@ -12,7 +12,7 @@ typealias RefreshToken = String
 
 extension UserDefaults {
     // 앞으로 여기에 key 값으로 추가 부탁드릴게요!
-    @OptionalUserDefault(key: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJiYW5nd2lkYWUiLCJleHAiOjE3NTQyMzE0MTQsInVzZXJJZCI6IjYyZDdmNDc3NmFkOTZjNTFkNDMzMGVhMiJ9.qYld9Je775prztT4oGWZ-4FDYg27TVJ24h1mQZG0fiE")
+    @OptionalUserDefault(key: "accessToken")
     static var accessToken: AccessToken?
     
     @OptionalUserDefault(key: "refreshToken")
