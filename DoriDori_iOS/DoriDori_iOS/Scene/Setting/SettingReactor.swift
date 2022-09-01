70b433fe2cd90946b4505de37a9b91be67c4f740
seoyoung <bgd216@naver.com>
2022-09-02 00:59:01 +0900

    설정 > 관리자에게 질문하기 navigate

--- DoriDori_iOS/DoriDori_iOS/Scene/Setting/SettingReactor.swift
+++ DoriDori_iOS/DoriDori_iOS/Scene/Setting/SettingReactor.swift
@@ -32,6 +32,7 @@ final class SettingReactor: Reactor {
         @Pulse var showAlert: AlertModel?
         @Pulse var shouldDismissPresentedViewController: Void?
         @Pulse var goToWelcomeViewController: Void?
+        @Pulse var navigateToAdminPage: Void?
     }
     
     // MARK: - Properties
@@ -102,6 +103,8 @@ final class SettingReactor: Reactor {
                                                           message: "작성한 질문, 댓글은 삭제되지 않아요!",
                                                           normalAction: AlertAction(title: "취소", action: { self.action.onNext(.didTapDenyCancel) }),
                                                           emphasisAction: AlertAction(title: "탈퇴", action: { self.action.onNext(.didTapWithdraw) }))
+            case .questionToAdmin:
+                _state.navigateToAdminPage = ()
             default: break
             }
         case .shouldDismissPresentedViewController:
