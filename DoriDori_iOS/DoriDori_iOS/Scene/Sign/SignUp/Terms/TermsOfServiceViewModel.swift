d3057ef0f418a36e81e4293df1e699e834ef357f
julia0926 <kj980926@naver.com>
2022-09-22 20:18:15 +0900

    ✨ 개인정보처리방침 동의약관 추가

--- DoriDori_iOS/DoriDori_iOS/Scene/Sign/SignUp/Terms/TermsOfServiceViewModel.swift
+++ DoriDori_iOS/DoriDori_iOS/Scene/Sign/SignUp/Terms/TermsOfServiceViewModel.swift
@@ -13,9 +13,11 @@ final class TermsOfServiceViewModel {
     private let repostiory: SignUpRepository = .init()
     private let allAgreeRelay: BehaviorRelay<Bool> = .init(value: false)
     private let useAgreeRelay: BehaviorRelay<Bool> = .init(value: false)
+    private let personalInfoAgreeRelay: BehaviorRelay<Bool> = .init(value: false)
     private let locationAgreeRelay: BehaviorRelay<Bool> = .init(value: false)
     
     private let useContentRelay = PublishRelay<TermsModel>()
+    private let personalContentRelay = PublishRelay<TermsModel>()
     private let locationContentRelay = PublishRelay<TermsModel>()
     
     private var disposeBag = DisposeBag()
@@ -23,23 +25,27 @@ final class TermsOfServiceViewModel {
     struct Input {
         let allAgree: Observable<Void>
         let useAgree: Observable<Void>
+        let personalInfoAgree: Observable<Void>
         let locationAgree: Observable<Void>
     }
     
     struct Output {
         let allAgreeOutput: Driver<Bool>
         let useAgreeOutput: Driver<Bool>
+        let personalInfoAgreeOutput: Driver<Bool>
         let locationAgreeOutput: Driver<Bool>
         let buttonOutput: Observable<Bool>
 
         let useContent: Observable<TermsModel>
         let locationContent: Observable<TermsModel>
+        let personalInfoContent: Observable<TermsModel>
     }
     
     func transform(input: Input) -> Output {
         
         var all = self.allAgreeRelay.value
         var use = self.useAgreeRelay.value
+        var personalInfo = self.personalInfoAgreeRelay.value
         var location = self.locationAgreeRelay.value
         
         input.allAgree
@@ -48,28 +54,41 @@ final class TermsOfServiceViewModel {
                 all.toggle()
                 use = all
                 location = all
+                personalInfo = all
                 self.allAgreeRelay.accept(all)
                 self.useAgreeRelay.accept(use)
+                self.personalInfoAgreeRelay.accept(personalInfo)
                 self.locationAgreeRelay.accept(location)
             }).disposed(by: disposeBag)
         
         input.useAgree.bind(onNext: { [weak self] _ in
             use.toggle()
             if !use { all = false }
-            else if use && location { all = true }
+            else if use && location && personalInfo { all = true }
             self?.allAgreeRelay.accept(all)
             self?.useAgreeRelay.accept(use)
+            self?.personalInfoAgreeRelay.accept(personalInfo)
         }).disposed(by: disposeBag)
         
         input.locationAgree.bind(onNext: { [weak self] _ in
             location.toggle()
             if !location { all = false }
-            else if use && location {  all = true }
+            else if use && location && personalInfo { all = true }
             self?.allAgreeRelay.accept(all)
             self?.locationAgreeRelay.accept(location)
+            self?.personalInfoAgreeRelay.accept(personalInfo)
         }).disposed(by: disposeBag)
         
-        let buttonOutput = Observable.combineLatest(allAgreeRelay, useAgreeRelay, locationAgreeRelay) { $0 && $1 && $2 }
+        input.personalInfoAgree.bind(onNext: { [weak self] _ in
+            personalInfo.toggle()
+            if !personalInfo { all = false }
+            else if use && location && personalInfo { all = true }
+            self?.allAgreeRelay.accept(all)
+            self?.locationAgreeRelay.accept(location)
+            self?.personalInfoAgreeRelay.accept(personalInfo)
+        }).disposed(by: disposeBag)
+        
+        let buttonOutput = Observable.combineLatest(allAgreeRelay, useAgreeRelay, locationAgreeRelay, personalInfoAgreeRelay) { $0 && $1 && $2 && $3 }
                 
         let terms = self.repostiory.fetchTermsOfService().share()
         
@@ -89,13 +108,23 @@ final class TermsOfServiceViewModel {
                                   necessary: model.necessary)
             }
         
+        let personalInfoContent = terms.compactMap { $0[safe: 2] }
+            .map { model in
+                return TermsModel(id: model.id,
+                                  title: model.title,
+                                  content: model.content,
+                                  necessary: model.necessary)
+            }
+        
         
         return Output(allAgreeOutput: allAgreeRelay.asDriver(),
                       useAgreeOutput: useAgreeRelay.asDriver(),
+                      personalInfoAgreeOutput: personalInfoAgreeRelay.asDriver(),
                       locationAgreeOutput: locationAgreeRelay.asDriver(),
                       buttonOutput: buttonOutput,
                       useContent: useContent,
-                      locationContent: locationContent)
+                      locationContent: locationContent,
+                      personalInfoContent: personalInfoContent)
     }
  
 }
